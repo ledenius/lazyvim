ea9e9eb13815c1bfcb5e82ee01312fd17469e86c
Mikko Aaltonen <mikko.aaltonen@krypta.fi>
2025-12-03 23:01:04 +0200

    first config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<leader>tt",
+  "<cmd>ToggleTerm direction=float name=666<cr>",
+  { desc = "Open a floating terminal." }
+)
+vim.keymap.set("t", "<M-t>", "<C-\\><C-n><C-w>k", { desc = "Escape terminal mode." })
